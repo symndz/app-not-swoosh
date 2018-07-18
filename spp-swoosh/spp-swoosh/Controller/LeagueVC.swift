5bcd6555daf39d3d383bc60df2074a20bc27a75b
training <marcin.danilewicz@mobica.com>
2018-07-18 17:45:22 +0200

    lesson8 programmatic loading segue

--- spp-swoosh/spp-swoosh/Controller/LeagueVC.swift
+++ spp-swoosh/spp-swoosh/Controller/LeagueVC.swift
@@ -15,12 +15,11 @@ class LeagueVC: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
+    @IBAction func onNextTapped(_ sender: Any) {
+        //programmatic segue call
+        performSegue(withIdentifier: "skillsVCSegue", sender: self)
+    }
 
     /*
     // MARK: - Navigation
