974e80bfd98e735425202e7b069cce841b497aa4
training <marcin.danilewicz@mobica.com>
2018-07-18 14:36:31 +0200

    pixel perfect aka working with frames

--- spp-swoosh/spp-swoosh/ViewController.swift
+++ spp-swoosh/spp-swoosh/ViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var swooshLogo: UIImageView!
+    @IBOutlet weak var bgImg: UIImageView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        swooshLogo.frame = CGRect(x: view.frame.size.width/2 - swooshLogo.frame.size.width/2,
+                                  y: 50,
+                                  width: swooshLogo.frame.size.width,
+                                  height: swooshLogo.frame.size.height)
+        bgImg.frame = self.view.frame
+        
     }
 
     override func didReceiveMemoryWarning() {
