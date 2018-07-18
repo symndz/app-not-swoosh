bbdb4c0cef836d968cfb796c64cd9046eb33a5a2
training <marcin.danilewicz@mobica.com>
2018-07-18 17:27:22 +0200

    lesson5-6

--- spp-swoosh/spp-swoosh/Controller/WelcomeVC.swift
+++ spp-swoosh/spp-swoosh/Controller/WelcomeVC.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WelcomeVC: UIViewController {
 
     
     @IBOutlet weak var swooshLogo: UIImageView!
@@ -22,9 +22,8 @@ class ViewController: UIViewController {
         
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue) {
+        
     }
 
 
