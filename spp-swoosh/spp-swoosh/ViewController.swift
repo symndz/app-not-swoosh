a12f346b1de2260effd99d4b1be047aff57f14f0
training <marcin.danilewicz@mobica.com>
2018-07-18 16:16:14 +0200

    intro auto laytout support

--- spp-swoosh/spp-swoosh/ViewController.swift
+++ spp-swoosh/spp-swoosh/ViewController.swift
@@ -18,11 +18,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        swooshLogo.frame = CGRect(x: view.frame.size.width/2 - swooshLogo.frame.size.width/2,
-                                  y: 50,
-                                  width: swooshLogo.frame.size.width,
-                                  height: swooshLogo.frame.size.height)
-        bgImg.frame = self.view.frame
+
         
     }
 
